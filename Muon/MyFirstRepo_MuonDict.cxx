eb58de77fe195b051b27a0b31afabab0a65319a0
Jonathan Karp <jkarp314@houston.nevis.columbia.edu>
2016-06-07 16:29:40 -0400

    anything

--- Muon/MyFirstRepo_MuonDict.cxx
+++ Muon/MyFirstRepo_MuonDict.cxx
@@ -39,6 +39,7 @@ namespace std {} using namespace std;
 
 // Header files passed as explicit arguments
 #include "MichelSpectrum.h"
+#include "MuonDecay.h"
 
 // Header files passed via #pragma extra_include
 
@@ -88,6 +89,51 @@ namespace ROOT {
 } // end of namespace ROOT
 
 namespace ROOT {
+   static TClass *larlitecLcLMuonDecay_Dictionary();
+   static void larlitecLcLMuonDecay_TClassManip(TClass*);
+   static void *new_larlitecLcLMuonDecay(void *p = 0);
+   static void *newArray_larlitecLcLMuonDecay(Long_t size, void *p);
+   static void delete_larlitecLcLMuonDecay(void *p);
+   static void deleteArray_larlitecLcLMuonDecay(void *p);
+   static void destruct_larlitecLcLMuonDecay(void *p);
+
+   // Function generating the singleton type initializer
+   static TGenericClassInfo *GenerateInitInstanceLocal(const ::larlite::MuonDecay*)
+   {
+      ::larlite::MuonDecay *ptr = 0;
+      static ::TVirtualIsAProxy* isa_proxy = new ::TIsAProxy(typeid(::larlite::MuonDecay));
+      static ::ROOT::TGenericClassInfo 
+         instance("larlite::MuonDecay", "MuonDecay.h", 28,
+                  typeid(::larlite::MuonDecay), ::ROOT::Internal::DefineBehavior(ptr, ptr),
+                  &larlitecLcLMuonDecay_Dictionary, isa_proxy, 4,
+                  sizeof(::larlite::MuonDecay) );
+      instance.SetNew(&new_larlitecLcLMuonDecay);
+      instance.SetNewArray(&newArray_larlitecLcLMuonDecay);
+      instance.SetDelete(&delete_larlitecLcLMuonDecay);
+      instance.SetDeleteArray(&deleteArray_larlitecLcLMuonDecay);
+      instance.SetDestructor(&destruct_larlitecLcLMuonDecay);
+      return &instance;
+   }
+   TGenericClassInfo *GenerateInitInstance(const ::larlite::MuonDecay*)
+   {
+      return GenerateInitInstanceLocal((::larlite::MuonDecay*)0);
+   }
+   // Static variable to force the class initialization
+   static ::ROOT::TGenericClassInfo *_R__UNIQUE_(Init) = GenerateInitInstanceLocal((const ::larlite::MuonDecay*)0x0); R__UseDummy(_R__UNIQUE_(Init));
+
+   // Dictionary for non-ClassDef classes
+   static TClass *larlitecLcLMuonDecay_Dictionary() {
+      TClass* theClass =::ROOT::GenerateInitInstanceLocal((const ::larlite::MuonDecay*)0x0)->GetClass();
+      larlitecLcLMuonDecay_TClassManip(theClass);
+   return theClass;
+   }
+
+   static void larlitecLcLMuonDecay_TClassManip(TClass* ){
+   }
+
+} // end of namespace ROOT
+
+namespace ROOT {
    // Wrappers around operator new
    static void *new_larlitecLcLMichelSpectrum(void *p) {
       return  p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::larlite::MichelSpectrum : new ::larlite::MichelSpectrum;
@@ -108,10 +154,32 @@ namespace ROOT {
    }
 } // end of namespace ROOT for class ::larlite::MichelSpectrum
 
+namespace ROOT {
+   // Wrappers around operator new
+   static void *new_larlitecLcLMuonDecay(void *p) {
+      return  p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::larlite::MuonDecay : new ::larlite::MuonDecay;
+   }
+   static void *newArray_larlitecLcLMuonDecay(Long_t nElements, void *p) {
+      return p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::larlite::MuonDecay[nElements] : new ::larlite::MuonDecay[nElements];
+   }
+   // Wrapper around operator delete
+   static void delete_larlitecLcLMuonDecay(void *p) {
+      delete ((::larlite::MuonDecay*)p);
+   }
+   static void deleteArray_larlitecLcLMuonDecay(void *p) {
+      delete [] ((::larlite::MuonDecay*)p);
+   }
+   static void destruct_larlitecLcLMuonDecay(void *p) {
+      typedef ::larlite::MuonDecay current_t;
+      ((current_t*)p)->~current_t();
+   }
+} // end of namespace ROOT for class ::larlite::MuonDecay
+
 namespace {
   void TriggerDictionaryInitialization_libMyFirstRepo_Muon_Impl() {
     static const char* headers[] = {
 "MichelSpectrum.h",
+"MuonDecay.h",
 0
     };
     static const char* includePaths[] = {
@@ -127,6 +195,7 @@ namespace {
 #pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
 extern int __Cling_Autoloading_Map;
 namespace larlite{class __attribute__((annotate("$clingAutoload$MichelSpectrum.h")))  MichelSpectrum;}
+namespace larlite{class __attribute__((annotate("$clingAutoload$MuonDecay.h")))  MuonDecay;}
 )DICTFWDDCLS";
     static const char* payloadCode = R"DICTPAYLOAD(
 #line 1 "libMyFirstRepo_Muon dictionary payload"
@@ -137,11 +206,13 @@ namespace larlite{class __attribute__((annotate("$clingAutoload$MichelSpectrum.h
 
 #define _BACKWARD_BACKWARD_WARNING_H
 #include "MichelSpectrum.h"
+#include "MuonDecay.h"
 
 #undef  _BACKWARD_BACKWARD_WARNING_H
 )DICTPAYLOAD";
     static const char* classesHeaders[]={
 "larlite::MichelSpectrum", payloadCode, "@",
+"larlite::MuonDecay", payloadCode, "@",
 nullptr};
 
     static bool isInitialized = false;
